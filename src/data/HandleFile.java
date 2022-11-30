0106cdf70cea47fd41f1eae5fbc45d8378836676
secfake <anhtrankma@gmail.com>
2022-12-01 00:42:13 +0700

    them data

---
+++ src/data/HandleFile.java
@@ -0,0 +1,62 @@
+package data;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import shop.entity.Shop;
+
+import javax.sound.midi.Soundbank;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.lang.reflect.Type;
+
+public class HandleFile {
+    Gson gson = new Gson();
+
+    public void saveFile(Shop shop) {
+
+        String json = gson.toJson(shop);
+
+        FileOutputStream fos = null;
+
+        try {
+            fos = new FileOutputStream("shop.json");
+
+            byte[] data = json.getBytes("utf8");
+
+            fos.write(data);
+        } catch (Exception e) {
+            System.out.println("Lỗi dữ liệu");
+        } finally {
+            if (fos != null) {
+                try {
+                    fos.close();
+                } catch (Exception e) {
+                    System.out.println("Lỗi dữ liệu");
+                }
+            }
+        }
+
+    }
+
+    public void readFile(Shop shop) {
+        FileReader reader = null;
+        try {
+            File file = new File("shop.json");
+            reader = new FileReader(file);
+
+            Type type = new TypeToken<Shop>() {
+            }.getType();
+            shop = gson.fromJson(reader, type);
+
+        } catch (Exception e) {
+            System.out.println("Lỗi dữ liệu");
+        } finally {
+            try {
+                reader.close();
+            } catch (Exception e) {
+                System.out.println("Lỗi dữ liệu");
+            }
+        }
+    }
+}
