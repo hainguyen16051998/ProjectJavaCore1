f5222a66cb30d70a452e6fc9502b2b477f41ef54
Ringo7996 <linhnt.jpc@gmail.com>
2022-12-02 00:38:19 +0700

    create entity + inputInfo showInfo

---
+++ src/school/constant/SubjectType.java
@@ -0,0 +1,13 @@
+package school.constant;
+
+public enum SubjectType {
+    TECH("Tech"),
+    BUSINESS("Business"),
+    LANGUAGE("Language");
+
+    public String value;
+
+    SubjectType(String value) {
+        this.value = value;
+    }
+}
